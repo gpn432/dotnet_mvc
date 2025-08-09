7667f6ff5213c2c0ca864b58d0eedb6ce32e14f4
Gautham P Nair <gauthampnair50@gmail.com>
2025-08-09 13:55:57 +0530

    a link to home is given

--- obj/Debug/net9.0/Demo3.AssemblyInfo.cs
+++ obj/Debug/net9.0/Demo3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9846e595f344b54e986ceb453d63602d7ac75914")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
